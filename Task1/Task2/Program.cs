929a832bdc41c3a52dfb9dc9494cfa0c28b8c0a7
vlad-hrytsiv <54137412+vlad-hrytsiv@users.noreply.github.com>
2020-02-09 23:29:00 +0200

    Task 2 ready

---
+++ Task1/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long reachNumber = 4000000;
+            long currentNumber = 0;
+
+            long firstNumber = 0;
+            long secondNumber = 1;
+
+            long sum = 0;
+
+            while (currentNumber <= reachNumber)
+            {
+                if (currentNumber%2 == 0)
+                {
+                    sum += currentNumber;
+                }
+                currentNumber = firstNumber + secondNumber;
+
+                firstNumber = secondNumber;
+                secondNumber = currentNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
